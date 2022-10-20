730eaa61ffb9dc80c16d9f2027c836a0b7080bb3
Diego Braga <diegobraga@hertz.com>
2022-10-20 20:33:04 +0100

    added header in app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,18 @@
 import type { AppProps } from "next/app";
 import { globalStyles } from "../styles/global";
+import { Container, Header } from "../styles/pages/app";
 
 globalStyles();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Container>
+      <Header>
+        <img src="/logo.svg" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  );
 }
 
 export default MyApp;
