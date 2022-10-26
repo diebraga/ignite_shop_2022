80f3175a602b0d2d8a229e6fe811df5707702bba
Diego Braga <diegobraga@hertz.com>
2022-10-26 09:01:30 +0100

    added static path

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import type { GetServerSideProps, NextPage } from "next";
+import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import { HomeContainer, HomeProduct } from "../styles/pages/home";
@@ -9,7 +9,7 @@ import Stripe from "stripe";
 import "keen-slider/keen-slider.min.css";
 import Link from "next/link";
 
-type ProductType = {
+export type ProductType = {
   id: string;
   name: string;
   imageUrl: string;
@@ -37,7 +37,7 @@ const Home: NextPage<HomeProps> = ({ products }) => {
       <HomeContainer ref={sliderRef} className="keen-slider">
         {products.map((product) => {
           return (
-            <Link href={`/product/${product.id}`} key={product.id}>
+            <Link href={`/product/${product.id}`} key={product.id} prefetch={false}>
               <HomeProduct className="keen-slider__slide">
                 <Image src={product.imageUrl} width={520} height={480} alt="" />
 
@@ -56,7 +56,7 @@ const Home: NextPage<HomeProps> = ({ products }) => {
 
 export default Home;
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ["data.default_price"],
   });
@@ -77,5 +77,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       products,
     },
+    revalidate: 60 * 60 * 2,
   };
 };
--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,25 +1,41 @@
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
+import Image from "next/image";
+import { useRouter } from "next/router";
 import React from "react";
+import Stripe from "stripe";
+import { ProductType } from "..";
+import { stripe } from "../../lib/stripe";
 import {
   ImageContainer,
   ProductContainer,
   ProductDetails,
 } from "../../styles/pages/product";
 
-const Product: React.FC = () => {
+type ProductProps = {
+  product: ProductType;
+  description: string;
+};
+
+const Product: NextPage<ProductProps> = ({ description, product }) => {
+  const { isFallback } = useRouter();
+
+  if (isFallback) return <h1>Loading...</h1>;
   return (
     <ProductContainer>
-      <ImageContainer></ImageContainer>
+      <ImageContainer>
+        <Image
+          src={product.imageUrl}
+          alt={product.name}
+          width={520}
+          height={480}
+        />
+      </ImageContainer>
 
       <ProductDetails>
-        <h1>Camiseta x</h1>
-        <span>45.00</span>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
 
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          Necessitatibus voluptate nesciunt earum libero molestias temporibus
-          sequi in possimus dolor error atque, voluptates velit similique, a eum
-          ad voluptatum fugit repellat!
-        </p>
+        <p>{description}</p>
         <button>Buy now</button>
       </ProductDetails>
     </ProductContainer>
@@ -27,3 +43,35 @@ const Product: React.FC = () => {
 };
 
 export default Product;
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [{ params: { id: "prod_Mf4WxgkrTu2kku" } }],
+    fallback: true,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const productID = params?.id as string;
+
+  const product = await stripe.products.retrieve(productID, {
+    expand: ["default_price"],
+  });
+  const price = product.default_price as Stripe.Price;
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat("en-GB", {
+          style: "currency",
+          currency: "EUR",
+        }).format((price.unit_amount as number) / 100),
+      },
+      description: product.description,
+    },
+    revalidate: 60 * 60 * 1,
+  };
+};
